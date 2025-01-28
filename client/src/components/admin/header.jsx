6150827cc0e7dbc4d5b280c6fcb069fdeb2e8f01
zippostorm <toghkklli@gmail.com>
2025-01-28 13:38:44 +0200

    complete sidebar UI for admin view

--- client/src/components/admin/header.jsx
+++ client/src/components/admin/header.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Button } from "../ui/button";
 import { AlignJustify, LogOut } from "lucide-react";
 
-const AdminHeader = () => {
+const AdminHeader = ({ setOpen }) => {
   return (
     <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
-      <Button className="lg:hidden sm:block">
+      <Button onClick={() => setOpen(true)} className="lg:hidden sm:block">
         <AlignJustify />
         <span className="sr-only">Toggle Menu</span>
       </Button>
