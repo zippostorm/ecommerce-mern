9fcd1d22137952bfe3adda48cb3e2450be4b491c
zippostorm <toghkklli@gmail.com>
2025-03-13 14:36:09 +0200

    change cookie config

--- server/controllers/auth/auth.controller.js
+++ server/controllers/auth/auth.controller.js
@@ -69,7 +69,7 @@ export const loginUser = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: false,
+        secure: true,
       })
       .json({
         success: true,
