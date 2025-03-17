f2affc292c32db1f789d0687df27f9bf492c579a
zippostorm <toghkklli@gmail.com>
2025-03-17 14:59:37 +0200

    some fix

--- server/server.js
+++ server/server.js
@@ -49,10 +49,10 @@ app.use("/api/shop/review", shopReviewRouter);
 app.use("/api/common/features", commonFeatureRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
